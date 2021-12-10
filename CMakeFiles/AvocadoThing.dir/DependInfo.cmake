6afa4792e2eb97ffebea928c519a846705a8b815
fernandezmatthew <80346555+fernandezmatthew@users.noreply.github.com>
2021-12-09 22:26:03 -0500

    Add files via upload

---
+++ CMakeFiles/AvocadoThing.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/nepat/CLionProjects/AvocadoThing/avocado.cpp" "C:/Users/nepat/CLionProjects/AvocadoThing/cmake-build-debug/CMakeFiles/AvocadoThing.dir/avocado.cpp.obj"
+  "C:/Users/nepat/CLionProjects/AvocadoThing/button.cpp" "C:/Users/nepat/CLionProjects/AvocadoThing/cmake-build-debug/CMakeFiles/AvocadoThing.dir/button.cpp.obj"
+  "C:/Users/nepat/CLionProjects/AvocadoThing/edge.cpp" "C:/Users/nepat/CLionProjects/AvocadoThing/cmake-build-debug/CMakeFiles/AvocadoThing.dir/edge.cpp.obj"
+  "C:/Users/nepat/CLionProjects/AvocadoThing/main.cpp" "C:/Users/nepat/CLionProjects/AvocadoThing/cmake-build-debug/CMakeFiles/AvocadoThing.dir/main.cpp.obj"
+  "C:/Users/nepat/CLionProjects/AvocadoThing/spriteButton.cpp" "C:/Users/nepat/CLionProjects/AvocadoThing/cmake-build-debug/CMakeFiles/AvocadoThing.dir/spriteButton.cpp.obj"
+  "C:/Users/nepat/CLionProjects/AvocadoThing/textBox.cpp" "C:/Users/nepat/CLionProjects/AvocadoThing/cmake-build-debug/CMakeFiles/AvocadoThing.dir/textBox.cpp.obj"
+  "C:/Users/nepat/CLionProjects/AvocadoThing/textureManager.cpp" "C:/Users/nepat/CLionProjects/AvocadoThing/cmake-build-debug/CMakeFiles/AvocadoThing.dir/textureManager.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Users/nepat/OneDrive/Desktop/SFML_Setup/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/AvocadoThing.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "AvocadoThing.exe"
+  "AvocadoThing.exe.manifest"
+  "AvocadoThing.pdb"
+  "CMakeFiles/AvocadoThing.dir/avocado.cpp.obj"
+  "CMakeFiles/AvocadoThing.dir/button.cpp.obj"
+  "CMakeFiles/AvocadoThing.dir/edge.cpp.obj"
+  "CMakeFiles/AvocadoThing.dir/main.cpp.obj"
+  "CMakeFiles/AvocadoThing.dir/spriteButton.cpp.obj"
+  "CMakeFiles/AvocadoThing.dir/textBox.cpp.obj"
+  "CMakeFiles/AvocadoThing.dir/textureManager.cpp.obj"
+  "libAvocadoThing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AvocadoThing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
