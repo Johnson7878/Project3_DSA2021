f342cd0e67c23299cbc67180cb239858fdca8213
fernandezmatthew <80346555+fernandezmatthew@users.noreply.github.com>
2021-12-09 22:26:46 -0500

    Add files via upload

---
+++ CMakeFiles/Avocados.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Avocados.exe"
+  "Avocados.exe.manifest"
+  "Avocados.pdb"
+  "CMakeFiles/Avocados.dir/main.cpp.obj"
+  "libAvocados.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Avocados.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
